c04a4518773edd1daa878bdc43f002590f0dbb45
Ycaro02 <Ycaro02@hotmail.fr>
2024-10-20 13:42:34 +0200

    Update ask user to overwrite dest repo

--- move_git_history.sh
+++ move_git_history.sh
@@ -5,23 +5,13 @@
 # Description: This script move all git history from one repo to another
 # Usage: ./move_git_history url_origine_repo url_destination_repo
 # You may have the right to push to the destination repo
+# Warning if the depo is not empty you will ask to force push and all data on the destination repo will be lost
 
 # Colors
 RED="\e[31m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
-BLUE="\e[34m"
-MAGENTA="\e[35m"
-CYAN="\e[36m"
-LIGHT_GRAY="\e[37m"
-GRAY="\e[90m"
-LIGHT_RED="\e[91m"
-LIGHT_GREEN="\e[92m"
-LIGHT_YELLOW="\e[93m"
-LIGHT_BLUE="\e[94m"
 LIGHT_MAGENTA="\e[95m"
-LIGHT_CYAN="\e[96m"
-WHITE="\e[97m"
 RESET="\e[0m"
 
 display_color_msg() {
@@ -38,14 +28,27 @@ display_double_color_msg () {
 	echo -e "${COLOR1}${MSG1}${RESET}${COLOR2}${MSG2}${RESET}"
 }
 
+# Function to check if the destination repo is empty
+is_repo_empty() {
+    git ls-remote "$1" > /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        display_color_msg ${RED} "Error accessing the destination repo"
+        exit 1
+    fi
+    if [[ -z "$(git ls-remote "$1")" ]]; then
+        return 0
+	fi
+	return 1
+}
 
-if [ -z "$1" ]; then
+
+if [[ -z "$1" ]]; then
 	display_color_msg ${RED} "Origine repo name is required" 
 	display_color_msg ${YELLOW} "Usage ./move_git_history url_origine_repo url_destination_repo"
     exit 1
 fi
 
-if [ -z "$2" ]; then
+if [[ -z "$2" ]]; then
 	display_color_msg ${RED} "Destination repo name is required" 
 	display_color_msg ${YELLOW} "Usage ./move_git_history url_origine_repo url_destination_repo"
 	exit 1
@@ -62,11 +65,10 @@ ORI_NAME="ori"
 
 display_color_msg ${YELLOW} "Clone ${ORI_REPO} to ${ORI_NAME}"
 
-git clone ${ORI_REPO} ${ORI_NAME}
+git clone ${ORI_REPO} ${ORI_NAME} > /dev/null 2>&1
 cd ${ORI_NAME}
 
 display_color_msg ${YELLOW} "Fetch tags"
-
 
 git fetch --tags
 
@@ -75,10 +77,24 @@ display_color_msg ${YELLOW} "Remote rm/add origin"
 git remote rm origin
 git remote add origin ${DEST_REPO}
 
-display_color_msg ${YELLOW} "Push origin --all and --tags"
+# Check if the destination repo is empty
+if ! is_repo_empty "${DEST_REPO}"; then
+    display_color_msg ${RED} "The destination repo is not empty."
+    read -p "Do you want to force push and overwrite the destination repo? (yes/no): " choice
+    if [ "$choice" != "yes" ]; then
+        display_color_msg ${LIGHT_MAGENTA} "Operation cancelled by the user."
+		rm -rf ${ORI_NAME}
+        exit 0
+    fi
+    FORCE_PUSH="--force"
+else
+    FORCE_PUSH=""
+fi
 
-git push origin --all
-git push --tags
+display_color_msg ${YELLOW} "Push ${FORCE_PUSH} origin --all and --tags"
+
+git push ${FORCE_PUSH} origin --all
+git push ${FORCE_PUSH} --tags
 
 # return to last dir to remove ori name
 cd .. 
